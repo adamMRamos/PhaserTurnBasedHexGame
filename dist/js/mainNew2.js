687306b798c1fdf39739cfa0aab3f00355c7e1e5
adamMRamos <amramos0828@gmail.com>
2017-11-20 19:24:56 -0500

    refactored the main class to only use board class for referencing the board and hexes.

--- dist/js/mainNew2.js
+++ dist/js/mainNew2.js
@@ -5,8 +5,9 @@ let assetLoader =  new AssetLoader();
 let hexSize = new Point(40,40);
 let origin = new Point(100,100);
 let layout = new Layout(Layout.FLAT, hexSize, origin);
-let hexMapGroup;
-let hexMap;
+let hexBoard;
+//let hexMapGroup;
+//let hexMap;
 let marker;
 
 function preload() {
@@ -23,58 +24,59 @@ function create() {
 
     cursors = game.input.keyboard.createCursorKeys();
 
-    hexMapGroup = game.add.group();
-    hexMap = HexMap.rectangleMap(12,7);
-    hexMap.forEach(hex => {
-        let centerOfHex = translator.hexToPixel(layout, hex);
-        const addHex = addHexToScreen(game, centerOfHex);
-        hexMapGroup.add(addHex);
-    });
-    hexMapGroup.x = origin.x;
-    hexMapGroup.y = origin.y;
+    hexBoard = new HexBoard(createHexMapAndHexGroup(12, 7), origin);
 
     marker = initializeHexMarker(game);
-    hexMapGroup.add(marker);
+    hexBoard.addObjectAtHex(marker, new Hex(0,0));
     moveIndex = game.input.addMoveCallback(updateHexMarker, this);
     console.log('moveIndex: ' + moveIndex);
 
-    const coordinateToPlaceUnit = translator.hexToPixel(layout, new Hex(10, 0));
-    const unit = new Unit(
-        game, coordinateToPlaceUnit.x, coordinateToPlaceUnit.y, assetLoader.validAssetNames.cube.tag);
-    hexMapGroup.add(unit);
+    const unit = new Unit(game, 0, 0, assetLoader.validAssetNames.cube.tag);
+    hexBoard.addObjectAtHex(unit, new Hex(10, 0));
 
     game.input.onTap.add(() => {
+        const hexBoardPosition = hexBoard.currentPosition();
         console.log('TAP');
-        console.log((game.input.x-hexMapGroup.x)+','+(game.input.y-hexMapGroup.y));
-        const tappedPoint = new Point(game.input.x-hexMapGroup.x, game.input.y-hexMapGroup.y);
-        const tappedHex = Hex.roundHex(translator.pixelToHex(layout, tappedPoint));
-        const newPoint = translator.hexToPixel(layout,tappedHex);
+        console.log((game.input.x-hexBoardPosition.x)+','+(game.input.y-hexBoardPosition.y));
+        const newPoint = translator.hexToPixel(layout, hexBoard.toBoardHex(game.input.x, game.input.y));
         unit.x = newPoint.x;
         unit.y = newPoint.y;
     });
 }
 
 function update() {
-    if (cursors.up.isDown) hexMapGroup.y += 8;
-    else if (cursors.down.isDown) hexMapGroup.y -= 8;
-    if (cursors.left.isDown) hexMapGroup.x += 8;
-    else if (cursors.right.isDown) hexMapGroup.x -= 8;
+    if (cursors.up.isDown) hexBoard.addXY(0, -8);
+    else if (cursors.down.isDown) hexBoard.addXY(0, +8);
+    if (cursors.left.isDown) hexBoard.addXY(-8, 0);
+    else if (cursors.right.isDown) hexBoard.addXY(+8, 0);
 }
 
 function render() {
+    const hexBoardPosition = hexBoard.currentPosition();
     game.debug.cameraInfo(game.camera, 32, 32);
     game.debug.text(game.input.x + ' x ' + game.input.y, 32, 20);
 
-    const selectedHexPoint = new Point(game.input.x-hexMapGroup.x, game.input.y-hexMapGroup.y);
+    const selectedHexPoint = new Point(game.input.x-hexBoardPosition.x, game.input.y-hexBoardPosition.y);
     const selectedHex = Hex.roundHex(translator.pixelToHex(layout, selectedHexPoint));
     game.debug.text('Hex '+selectedHex.x+', '+selectedHex.y, 200, 20);
 
     const selectedOffsetHex = OffsetCoordinate.hexToQOffset(OffsetCoordinate.ODD, selectedHex);
     game.debug.text('OffsetHex '+selectedOffsetHex.x+', '+selectedOffsetHex.y, 200, 40);
 
-    game.debug.text('HG Mouse pos: '+(game.input.x-hexMapGroup.x)+', '+(game.input.y-hexMapGroup.y), 32, 120);
-    game.debug.text('Board pos: '+(hexMapGroup.x)+', '+(hexMapGroup.y), 32, 140);
+    game.debug.text('HG Mouse pos: '+(game.input.x-hexBoardPosition.x)+', '+(game.input.y-hexBoardPosition.y), 32, 120);
+    game.debug.text('Board pos: '+(hexBoardPosition.x)+', '+(hexBoardPosition.y), 32, 140);
     game.debug.text('MarkerLoc: '+(marker.x)+', '+(marker.y), 32, 160);
+}
+
+function createHexMapAndHexGroup(xTiles, yTiles) {
+    const hexMapGroup = game.add.group();
+    const hexMap = HexMap.rectangleMap(xTiles, yTiles);
+    hexMap.forEach(hex => {
+        let centerOfHex = translator.hexToPixel(layout, hex);
+        const addHex = addHexToScreen(game, centerOfHex);
+        hexMapGroup.add(addHex);
+    });
+    return {group: hexMapGroup, map: hexMap};
 }
 
 function addHexToScreen(game, centerOfHex) {
@@ -94,9 +96,8 @@ function initializeHexMarker(game) {
 
 function updateHexMarker() {
     marker.visible = true;
-    const mousePosition = new Point(game.input.worldX-hexMapGroup.x, game.input.worldY-hexMapGroup.y);
-    const hexPosition = translator.pixelToHex(layout, mousePosition);
-    const centerOfHexAsPixel = translator.hexToPixel(layout, Hex.roundHex(hexPosition));
+    const centerOfHexAsPixel =
+        translator.hexToPixel(layout, hexBoard.toBoardHex(game.input.worldX, game.input.worldY));
     marker.x = centerOfHexAsPixel.x;
     marker.y = centerOfHexAsPixel.y;
 }
